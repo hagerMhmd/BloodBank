3a515c01b9852f5690abb7ffc3d738669f2dc392
hagerMhmd <hagermuhammedibrahem@gmail.com>
2022-03-20 20:47:06 +0200

    link the packages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Components/About/About';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import LastesNews from './Components/LastesNews/LastesNews';
 import NavBar from './Components/NavBar/NavBar';
-import SuportUs from './Components/SupportUs/SuportUs';
+import SupportUs from './Components/SupportUs/SupportUs';
 import Testimonials from './Components/Testimonials/Testimonials';
 
 function App() {
   return <>
-  <NavBar/>
-  <Home/>
-  <About/>
-  {/* Component Dina msh 3arfa asmo eh */}
-  <SuportUs/>
-  <LastesNews/>
-  <Testimonials/>
-  <Footer/>
+    <NavBar />
+    <Routes>
+      <Route path='/home' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/supportus' element={<SupportUs />} />
+      <Route path='/lastesnews' element={<LastesNews />} />
+      <Route path='/testimonials' element={<Testimonials />} />
+    </Routes>
+    <Footer />
   </>
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <App />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
 
