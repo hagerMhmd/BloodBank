cd2e29525e434d4e2867e8057b4d78ae012058db
hagerMhmd <hagermuhammedibrahem@gmail.com>
2022-03-21 19:03:04 +0200

    Donate Box

--- src/Components/DonateBox/DonateBox.jsx
+++ src/Components/DonateBox/DonateBox.jsx
@@ -1,20 +1,60 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import style from './DonateBox.module.css'
-
+import icon1 from '../../Imgs/icon-01.png'
+import icon2 from '../../Imgs/icon-02.png'
+import icon3 from '../../Imgs/icon-03.png'
+import icon4 from '../../Imgs/icon-04.png'
 export default function DonateBox() {
+
     return <>
         <div className={`${style.donateColor}`}>
             <div className="container text-white text-center">
                 <div className="row">
-                    <Link to='/about' className={`text-decoration-none m-0 p-0 text-white`}>
-                        <div className={`${style.donatesHover} col-md-3`}>
-                            <i className={`${style.donateIcon} py-5 fa-solid fa-hand-holding-medical text-white`}></i>
-                            <h1 className='pb-5'>Blood Donor <br /> Basics</h1>
-                        </div>
-                    </Link>
+                    <div className={`col-md-3 ${style.donatesHover} `}>
+                        <Link to='/about' className={`text-decoration-none m-0 text-white`}>
+                            <img src={icon1} className='py-5 my-2' alt="" />
+                            <h1 className='text-uppercase pb-5 mb-0'>Blood Donor basics </h1>
+                        </Link>
+                    </div>
+                    <div className={`col-md-3 ${style.donatesHover} `}>
+                        <Link to='/about' className={`text-decoration-none m-0 text-white`}>
+                            <img src={icon2} className='py-5' alt="" />
+                            <h1 className='text-uppercase pb-5 mb-0 '>Target Your Type®</h1>
+                        </Link>
+                    </div>
+                    <div className={`col-md-3 ${style.donatesHover} `}>
+                        <Link to='/about' className={`text-decoration-none m-0 text-white`}>
+                            <img src={icon3} className='py-5' alt="" />
+                            <h1 className='text-uppercase pb-5 mb-0'>Donate <br /> Now </h1>
+                        </Link>
+                    </div>
+                    <div className={`col-md-3 ${style.donatesHover} `}>
+                        <Link to='/about' className={`text-decoration-none m-0 text-white`}>
+                            <img src={icon4} className='py-5' alt="" />
+                            <h1 className='text-uppercase pb-5 mb-0'>Share Your <br /> story </h1>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
     </>
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
